e29b362c6235c071b5e92e48f0c0522d6ae43928
a.utyanskiy <a.utyanskiy@rambler-co.ru>
2025-07-05 00:20:14 +0300

    task 5

---
+++ apps/temperature-api/Controllers/TemperatureController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TemperatureApi.Controllers
+{
+    [ApiController]
+    public class TemperatureController : Controller
+    {
+        /// <summary>
+        /// Retrieves temperature by location
+        /// </summary>
+        /// <response code="200">Temperature retreived</response>
+        [HttpGet("temperature")]
+        public IActionResult GetTemperature([FromQuery] string location)
+        {
+            var temperature = new Random().Next(-10, 30);
+
+            return Ok(temperature);
+        }
+    }
+}
